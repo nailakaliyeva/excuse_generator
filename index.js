2683e9f9ffcc9afcb64a9053f4df488c84efb9b3
Naila <nailakaliyeva@gmail.com>
2019-07-09 15:46:52 +0000

    separated js

---
+++ index.js
@@ -0,0 +1,12 @@
+        let noun = ["My cat ", "My dog ", "My iguana "];
+        let verb = ["ate ", "chewed up ", "destroyed "];
+        let object = ["my homework ", "my project ", "my book "];
+        let adverb = ["yesterday ", "in the morning "];
+
+        let rNoun = noun[Math.floor(noun.length*Math.random())];
+        let rVerb = verb[Math.floor(verb.length*Math.random())];
+        let rObject = verb[Math.floor(object.length*Math.random())];
+        let rAdverb = verb[Math.floor(adverb.length*Math.random())];
+
+        let theEntireExcuse = rNoun + rVerb + rObject + rAdverb;
+        document.getElementById("excuse").innerHTML = theEntireExcuse;
